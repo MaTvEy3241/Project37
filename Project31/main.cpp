6ee93d4dbeb28f93d1c8d80badda11fd239506d0
EvgenDev09 <eugene.lippa@gmail.com>
2024-02-01 18:21:11 +0300

    Add sum function

--- Project31/main.cpp
+++ Project31/main.cpp
@@ -1,11 +1,10 @@
 #include <iostream>
 #include  <string>
-
+int sum(int a, int b);
 using namespace std;
-
 int main() {
 
-	cout << "lesson 31" << endl;
+	cout << sum(3,7) << endl;
 
 	return 0;
 }
