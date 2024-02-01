606bae91005814a54b52c555d7851e1cd644bebe
Prosstik <110463504+MaTvEy3241@users.noreply.github.com>
2024-02-01 18:35:38 +0300

    Update main.cpp

--- Project31/main.cpp
+++ Project31/main.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
 #include  <string>
-int sum(int a, int b);
+int sum(int a, int b, int c);
 using namespace std;
 int main() {
 
-	cout << sum(3,7) << endl;
+	cout << sum(3,7,5) << endl;
 
 	return 0;
 }
